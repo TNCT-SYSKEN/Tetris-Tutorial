0dad654a4c6701197f2ab7ebdc71582770a3421e
yato0421 <raizaraiza777@gmail.com>
2018-11-08 01:07:21 +0900

    各関数の機能コメントの追加

--- Tetris_sol/Tetris/Main.cpp
+++ Tetris_sol/Tetris/Main.cpp
@@ -2,15 +2,15 @@
 # include <Siv3D.hpp> // OpenSiv3D v0.3.0
 
 //関数のプロトタイプ宣言
-//全体の流れの管理する関数
+//ゲーム全体の流れの管理する関数
 void gameManager();
-//ミノに関する処理を行う関数
+//ミノ関連の処理を行う関数
 void moveMino();
 void spinMino();
 void putMino();
 void eraseLine();
 int* makeMino();
-//画面表示に関する処理を行う関数
+//描写関連の処理を行う関数
 void drawManager();
 void drawMap();
 void drawMino();
@@ -36,43 +36,83 @@ void Main()
 	}
 }
 
+/*
+渡し値：
+機能：ゲーム全体の流れを管理
+*/
 void gameManager()
 {
 }
 
+/*
+渡し値：
+機能：上下左右移動 , 一定時間ごとにミノを下移動
+*/
 void moveMino()
 {
 }
 
+/*
+渡し値：
+機能：ミノの右・左回転
+*/
 void spinMino()
 {
 }
 
+/*
+渡し値：
+機能：ミノをゲーム画面上に配置
+*/
 void putMino()
 {
 }
 
+/*
+渡し値：
+機能：ミノが横一列揃ったら消す
+*/
 void eraseLine()
 {
 }
 
+/*
+渡し値：
+機能：ミノをランダムで自動生成
+*/
 int * makeMino()
 {
 	return nullptr;
 }
 
+/*
+渡し値：
+機能：描写関連の処理を管理
+*/
 void drawManager()
 {
 }
 
+/*
+渡し値：
+機能：ゲーム画面の描写
+*/
 void drawMap()
 {
 }
 
+/*
+渡し値：
+機能：ミノの描写
+*/
 void drawMino()
 {
 }
 
+/*
+渡し値：なし
+機能：背景の描写
+*/
 void drawBackGround()
 {
 }
